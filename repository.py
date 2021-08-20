a89ae1f899f9663e379b6514adb9a41b323bd91a
Baptiste Maingret <baptiste.maingret@gmail.com>
2021-08-20 20:36:33 +0200

    Adding repository impl

--- repository.py
+++ repository.py
@@ -1,13 +1,21 @@
 from typing import Protocol
-from allocation import Batch
+from allocation import Batch, OrderLine
+from abc import abstractmethod
 
 class AbstractRepository(Protocol):
-    def add(batch: Batch):
+    def add(self, batch: Batch) -> None:
         pass
 
-    def get(batch: Batch):
+    @abstractmethod
+    def get(self, batch_reference: str) -> Batch:
         pass
 
 class SQLiteInMemoryRepository():
-    
+    def __init__(self, session) -> None:
+        self.session = session
 
+    def add(self, batch: Batch) -> None:
+        self.session.add(batch)
+
+    def get(self, batch_reference: str) -> Batch:
+        return self.session.query(Batch).filter(Batch.reference==batch_reference).one()
