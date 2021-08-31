6b8644d3922873f4696fd73171931a9c677d2799
Baptiste Maingret <baptiste.maingret@gmail.com>
2021-08-31 18:01:16 +0200

    adding fakerepository straight from the book

--- allocations/repository.py
+++ allocations/repository.py
@@ -31,3 +31,17 @@ class SQLiteInMemoryRepository:
 
     def list(self) -> List[Batch]:
         return self.session.query(Batch).all()
+
+
+class FakeRepository:
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
